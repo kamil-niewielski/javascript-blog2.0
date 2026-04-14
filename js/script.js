38b5399686c56577f418cb2622fcd8b083223e3a
Kamil Niewielski <nevel1906.kn@gmail.com>
2026-04-14 11:38:09 +0100

    przed dodaniem chmury script.js

--- js/script.js
+++ js/script.js
@@ -68,14 +68,18 @@ function generateTitleLinks(customSelector = ''){
 
 generateTitleLinks()
 
+const optTagsListSelector = ('.tags.list');
 const links = document.querySelectorAll('.titles a');
 console.log('links', links);
+
 
   for(let link of links){
   link.addEventListener('click', titleClickHandler);
   }
 
   function generateTags(){
+  /* [NEW] create a new variable allTags with an empty array */
+  let allTags = [];
   /* find all articles */
   const articles = document.querySelectorAll(optArticleSelector);
   /* START LOOP: for every article: */
@@ -98,12 +102,22 @@ console.log('links', links);
       console.log('linkHTML', linkHTML);
       /* add generated code to html variable */
       html = html + linkHTML
+      /* [NEW] check if this link is NOT already in allTags */
+      if(allTags.indexOf(linkHTML) == -1){
+        /* [NEW] add generated code to allTags array */
+        allTags.push(linkHTML);
+      }
     /* END LOOP: for each tag */
       }
     /* insert HTML of all the links into the tags wrapper */
   articleTagWrapper.innerHTML = html; 
   /* END LOOP: for every article: */
     }
+  /* [NEW] find list of tags in right column */
+  const tagList = document.querySelector(optTagsListSelector);
+
+  /* [NEW] add html from allTags to tagList */
+  tagList.innerHTML = allTags.join(' '); 
 }
 
 generateTags();
